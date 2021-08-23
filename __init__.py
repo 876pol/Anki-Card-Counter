d3e754812430d459568cb5e27cb4962c944576ef
876pol <77174148+876pol@users.noreply.github.com>
2021-08-23 11:21:28 -0400

    update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import json
-import random
 import os
 
+import anki.cards
 from aqt import mw, gui_hooks
 from aqt.qt import *
 
@@ -31,6 +31,7 @@ class CardCounter:
         # creates message box
         mb = QDialog(mw.app.activeWindow() or mw)
         mb.setFixedSize(307, 145)
+        mb.setWindowTitle("Anki - Card Counter")
         mb.setStyleSheet(
             """
             QLabel {
@@ -46,15 +47,6 @@ class CardCounter:
             """
         )
 
-        # 10% chance to say "Steven is a Noob"
-        mb.setWindowTitle(
-            "".join([chr(c - 2)
-                     for c in [85, 118, 103, 120, 103, 112, 34, 107, 117, 34, 99, 34, 80, 113, 113, 100, 1442]])
-            if random.randint(1, 10) == 10
-            else
-            "".join([chr(c - 2) for c in
-                     [67, 112, 109, 107, 34, 47, 34, 69, 99, 116, 102, 34, 69, 113, 119, 112, 118, 103, 116, 1644]]))
-
         # adds image
         image = QLabel(mb)
         image.setPixmap(QPixmap(os.path.join(os.path.dirname(__file__), "res", "icon.png")))
@@ -66,18 +58,20 @@ class CardCounter:
         text.move(115, 25)
 
         # adds reset button
-        def reset_func():
+        def reset_func() -> None:
             self.reset()
             text.setText("\nCards Reviewed: " + str(self.count["card_count"]))
+
         reset_button = QPushButton(mb)
         reset_button.setText("Reset")
         reset_button.move(15, 110)
         reset_button.clicked.connect(reset_func)
 
         # adds decrease button
-        def decrease_func():
+        def decrease_func() -> None:
             self.decrease()
             text.setText("\nCards Reviewed: " + str(self.count["card_count"]))
+
         decrease_button = QPushButton(mb)
         decrease_button.setText("Decrease 10")
         decrease_button.move(110, 110)
@@ -89,17 +83,10 @@ class CardCounter:
         ok_button.move(205, 110)
         ok_button.clicked.connect(lambda: mb.close())
 
-        # checks if steven is a noob
-        check = sum([ord(c) for c in mb.windowTitle()])
-        if check // 2 == ord(mb.windowTitle()[len(mb.windowTitle()) - 1]):
-            mb.setWindowTitle(mb.windowTitle()[:-1])
-            eval("execmb.run.mcexecmb.exec()mbexe steven is a noob lmfao"[
-                 17:26], {"mb": mb})
-        else:
-            raise RuntimeError("Uh Oh! Noobs can't mess with source code!")
+        mb.exec()
 
     # increments card count
-    def update(self, card) -> None:
+    def update(self, card: anki.cards.Card) -> None:
         self.count["card_count"] += 1
         f = open(self.file, "w")
         json.dump(self.count, f)
