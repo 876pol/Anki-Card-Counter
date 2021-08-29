5879ccb4e5584cc99094e893c2ed43ac43b2e8e7
876pol <77174148+876pol@users.noreply.github.com>
2021-08-28 20:21:38 -0400

    added sync with cloud

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from CardCounter import CardCounter
+from . import CardCounter
 
 # Anki Card Counter
 # Creates a new menu item under "Tools" that displays the number of cards that you have studied
